85bfc8f402f48ee4fbf54e927df7884ccaeefc48
raymond53 <62273326+raymond53@users.noreply.github.com>
2020-04-18 18:19:45 -0600

    Add files via upload

--- imdb_episode_ratings.py
+++ imdb_episode_ratings.py
@@ -5,9 +5,6 @@ import requests
 import seaborn as sns
 from bs4 import BeautifulSoup
 
-# Generate a heatmap of a tv series episodes
-
-
 # Input show title to find the imdb id
 
 # Variables
@@ -15,8 +12,7 @@ show = 'Doctor Who'
 url_search = 'https://www.imdb.com/find?q='
 
 # Set up Dataframe to create Heatmap
-df = pd.DataFrame(columns=['show', 'season', 'episode', 'rating'])
-df = df.astype({'show': 'str', 'season': 'int', 'episode': 'int', 'rating': 'float'})
+
 
 # Put the search term in the url_search     example: imdb.com/find?q=breaking+bad
 for word in show.split():
@@ -24,57 +20,64 @@ for word in show.split():
         url_search = url_search + word
     else:
         url_search = url_search + '+' + word
+# url_search
 
 # Parse the get request
-r_search = requests.get(url_search)
-soup_search = BeautifulSoup(r_search.text, 'html.parser')
+get_search = requests.get(url_search)
+soup_search = BeautifulSoup(get_search.text, 'html.parser')
 
-# Get the imdb id
-content_search = soup_search.find('tr', attrs={'class': 'findResult odd'})
-imdb_id = content_search.find('a')['href'].split('/')[2]
-show = content_search.find_all('a')[1].text
-print('title:' + show)
-print('imdb_id:' + imdb_id)
+# Get the imdb_id
+find_search = soup_search.find('tr', attrs={'class': 'findResult odd'})
+imdb_id = find_search.find('a')['href'].split('/')[2]
+show = find_search.find_all('a')[1].text
+print(show)
+print(imdb_id)
 
 # Use the imdb id to find the latest season available
-
-# Parse the get request
-r_home = requests.get('https://www.imdb.com/title/' + imdb_id)
-soup_home = BeautifulSoup(r_home.text, 'html.parser')
+get_home = requests.get('https://www.imdb.com/title/' + imdb_id)
+soup_home = BeautifulSoup(get_home.text, 'html.parser')
+# soup_home
 
 # find the latest season available
-content_home = soup_home.find('div', attrs={'class': 'seasons-and-year-nav'})
-max_season = int(content_home.find('a').text)
-print('seasons:' + str(max_season))
+find_home = soup_home.find('div', attrs={'class': 'seasons-and-year-nav'})
+max_season = int(find_home.find('a').text)
+max_season
 
 # Find the ratings for episodes in each season
+records = []
 for current_season in range(1, max_season + 1):
-    r_season = requests.get('https://www.imdb.com/title/' + imdb_id + '/episodes?season=' + str(current_season))
-    soup_season = BeautifulSoup(r_season.text, 'html.parser')
-    # soup_season
+    get_season = requests.get('https://www.imdb.com/title/' + imdb_id + '/episodes?season=' + str(current_season))
+    soup_season = BeautifulSoup(get_season.text, 'html.parser')
+
+    # List of ratings
+    find_season = soup_season.find_all('span', attrs={'class': 'ipl-rating-star__rating'})[
+                  ::23]  # ratings occur in every 23rd item
+
     episode = 1
-    results = soup_season.find_all('span', attrs={'class': 'ipl-rating-star__rating'})[
-              ::23]  # ratings occur in every 23rd item
-    for rating in results:
-        # print(current_season,episode,rating.text)
-        df = df.append({'show': show, 'season': current_season, 'episode': episode, 'rating': float(rating.text)},
-                       ignore_index=True)
+    for rating in find_season:
+        records.append((show, current_season, episode, float(rating.text)))
         episode += 1
+
+# Add records to DataFrame
+df = pd.DataFrame(records, columns=['show', 'season', 'episode', 'rating'])
 
 # Num of season and episodes determine size of plot
 max_season = df.season.max()
 max_episode = df.episode.max()
+print(df.episode.count())
 
 # Pivot Table
 df_pivot = df.pivot(index='season', columns='episode', values='rating')
+df_pivot
 
 # Plot
 f, ax = plt.subplots(figsize=(max_episode, max_season))
-ax = sns.heatmap(df_pivot,vmin=6,vmax=10, annot=True,linewidths=1, cmap='RdYlGn', linecolor='gray', cbar=False)
-ax.set_title(show,fontsize=30)
-ax.set_xlabel('Episode',fontsize=20,labelpad=15)
-ax.set_ylabel('Season',fontsize=20,labelpad=15)
+ax = sns.heatmap(df_pivot, vmin=6, vmax=10, annot=True, linewidths=1, cmap='RdYlGn', linecolor='gray', cbar=False)
+ax.set_title(show, fontsize=30)
+ax.set_xlabel('Episode', fontsize=20, labelpad=15)
+ax.set_ylabel('Season', fontsize=20, labelpad=15)
 ax.tick_params(labelsize=15)
 plt.yticks(rotation=0)
 plt.tight_layout()
-plt.savefig(show+'.png')
+plt.savefig(show + '.png')
+plt.show()
